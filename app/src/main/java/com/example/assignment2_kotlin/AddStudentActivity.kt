e30526f20a4d7068d021256baba1bd41ffde2955
PC <avital.sale@gmail.com>
2025-01-22 20:29:03 +0200

    added toolbar for add student screen

--- app/src/main/java/com/example/assignment2_kotlin/AddStudentActivity.kt
+++ app/src/main/java/com/example/assignment2_kotlin/AddStudentActivity.kt
@@ -1,12 +1,14 @@
 package com.example.assignment2_kotlin
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.assignment2_kotlin.model.Model
@@ -23,6 +25,12 @@ class AddStudentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // הגדרת ה-Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Add Student"
 
         val saveButton: Button = findViewById(R.id.add_student_activity_save_button)
         val cancelButton: Button = findViewById(R.id.add_student_activity_cancel_button)
@@ -50,5 +58,15 @@ class AddStudentActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish() // חזרה למסך הקודם
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
